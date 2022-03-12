c9bcecd397e81c36a92a89aee7b1fc08e3943e82
loeken <loeken@internetz.me>
2022-03-12 19:53:12 +0100

    version helper

---
+++ deploy/helpers/check_versions.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+function lastversion() {
+    version=$(curl -s https://artifacthub.io/api/v1/packages/helm/$1/$2/feed/rss | xmlstarlet sel -t -m "/rss/channel/item[1]" -v "title")
+    echo "$version"
+    mkdir -p versions/$1
+    currentVersion=`cat versions/$1/$2`
+    if [ "$currentVersion" == "" ]; then
+        echo $version > versions/$1/$2
+    fi
+}
+
+# plexversion=`lastversion k8s-at-home plex`
+# plexCurrentVersion=`cat versions/k8s-at-home/plex`
+# echo plex current: $plexCurrentVersion remote: $plexversion
+
+# certManager
+certManagerVersion=`lastversion cert-manager cert-manager`
+certManagerVersionCurrent=`cat versions/cert-manager/cert-manager`
+echo cert-manager current: $certManagerVersionCurrent remote: $certManagerVersion
